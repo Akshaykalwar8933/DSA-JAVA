d5840a2626cdd10928629dc6e849bcf73000665c
Akshay Kalwar <akshaykalwar@macbookM4.local>
2025-12-28 04:09:09 +0530

    check changes

--- day02/helloworld.java
+++ day02/helloworld.java
@@ -4,6 +4,7 @@ package  day02;
 public class helloworld {
     
     public static void main (String args[]){
-        System.out.println("Hello Akshay");
+        System.out.println("Hurrrrrr.....");
+        System.out.println("Hello Java on 28 December");
     }
 }
